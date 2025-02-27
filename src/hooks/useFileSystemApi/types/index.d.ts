5010d364ea17473f958118790c1337652cef2bf5
YarLama <gaideichuk1998@mail.ru>
2025-02-27 16:40:02 +0300

    #0.3. remove useless comment from types

--- src/hooks/useFileSystemApi/types/index.d.ts
+++ src/hooks/useFileSystemApi/types/index.d.ts
@@ -115,19 +115,6 @@ declare global {
     recursive?: boolean | undefined
   }
 
-  // type WriteParams =
-  //     | { type: 'write'; position?: number | undefined; data: BufferSource | Blob | string }
-  //     | { type: 'seek'; position: number }
-  //     | { type: 'truncate'; size: number };
-
-  // type FileSystemWriteChunkType = BufferSource | Blob | string | WriteParams;
-
-  // class FileSystemWritableFileStream extends WritableStream {
-  //     write(data: FileSystemWriteChunkType): Promise<void>;
-  //     seek(position: number): Promise<void>;
-  //     truncate(size: number): Promise<void>;
-  // }
-
   interface FileSystemFileHandle extends FileSystemHandle {
     readonly kind: 'file'
     getFile(): Promise<File>
